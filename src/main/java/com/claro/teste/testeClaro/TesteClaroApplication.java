d0dc254a3ec5a8300b0e149ee12ecf01176aaf40
unknown <upassos@gmail.com>
2021-09-01 16:24:17 -0300

    project structure

---
+++ src/main/java/com/claro/teste/testeClaro/TesteClaroApplication.java
@@ -0,0 +1,13 @@
+package com.claro.teste.testeClaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteClaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteClaroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/claro/teste/testeClaro/TesteClaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claro.teste.testeClaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteClaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
